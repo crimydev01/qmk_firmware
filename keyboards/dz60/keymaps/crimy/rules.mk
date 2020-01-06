9ed7d3c06de94ac9e8f69165415d79b5eb499cb5
Woohyun Kim <crimy00@yonsei.ac.kr>
2020-01-06 23:49:55 +0900

    2020.01.06. Added Crimy's DZ60 keymap

---
+++ keyboards/dz60/keymaps/crimy/rules.mk
@@ -0,0 +1,13 @@
+
+BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE = yes	# Mouse keys(+4700)
+EXTRAKEY_ENABLE = yes	# Audio control and System control(+450)
+CONSOLE_ENABLE = no	# Console for debug(+400)
+COMMAND_ENABLE = no    # Commands for debug and configuration
+SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
+NKRO_ENABLE = yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+BACKLIGHT_ENABLE = yes  # Enable per-key backlight LEDs
+AUDIO_ENABLE = no    # There is no speaker on this PCB
+RGBLIGHT_ENABLE = no # Disnable the RGB underglow LEDs
+
+
