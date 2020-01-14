aa28c5d8ead6fa81cba3aaa57fd6f7d96f711b7e
Woohyun Kim <crimy00@yonsei.ac.kr>
2020-01-14 14:12:57 +0900

    2020.01.14. Tokyo60 추가

--- keyboards/dz60/keymaps/crimy/config.h
+++ keyboards/dz60/keymaps/crimy/config.h
@@ -3,6 +3,10 @@
 
 #include "../../config.h"
 
+#undef	PRODUCT
+#define PRODUCT         [CRIMY] DZ60
+#undef	DESCRIPTION
+#define DESCRIPTION     DZ60 customized for Crimy
 // #define TAPPING_TERM 200
 // #define RETRO_TAPPING
 // #define PERMISSIVE_HOLD
---
+++ keyboards/tokyo60/keymaps/crimy/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+// place overrides here
+//
+#undef	PRODUCT
+#define PRODUCT         [CRIMY] Tokyo60
+#undef	DESCRIPTION
+#define DESCRIPTION     Tokyo60 customized for Crimy
+
+#endif
